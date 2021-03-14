68be6376459941b72980a41f36abe8eb0177c959
va-ovsyanikov <va.ovsyanikov@gmail.com>
2021-03-14 22:34:49 +0300

    Update server.js
    up

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
   
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./public/db.json');
+const router = jsonServer.router('public/db.json');
 const middlewares = jsonServer.defaults({
   static: './dist',
 });
